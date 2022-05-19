d9a3f9998ce6ae8903d6f2160a12d53b6317e9db
Omar De La Cruz <36172889@uaz.edu.mx>
2022-05-18 19:29:59 -0500

    Checale

---
+++ SistemaGeolocalizacion/centroinvestigacion/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class CentroInvestigacion(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+    direccion = models.CharField('Direccion', max_length=150, unique=True)
+    latitud = models.CharField(max_length=20, unique=True)
+    longitud = models.CharField(max_length=20, unique=True)
+    telefono = models.CharField('Telefono', max_length=10, unique=True)
+    
+    def __str__(self):
+        return self.nombre 
